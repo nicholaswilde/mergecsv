d6e54cfa0e4a7d6890be465e0203d2ea2b68b40d
nicholaswilde <ncwilde43@gmail.com>
2019-12-02 18:32:04 -0800

    Added flip function, fixed error in to_float32, and reformatted comments.

--- mergecsv/words.py
+++ mergecsv/words.py
@@ -19,15 +19,15 @@ REF_DATE = datetime(2001, 1, 1)
 
 class word:
     def __init__(self, value):
-        '''
+        """
         https://stackoverflow.com/a/4843178/1061279
-        '''
+        """
         if not isinstance(value, str): raise TypeError(value)
         self.value = str(value)
 
 
     def __repr__(self):
-        '''https://stackoverflow.com/a/1984177'''
+        """https://stackoverflow.com/a/1984177"""
         return(self.value)
 
 
@@ -37,19 +37,19 @@ class word:
 
     @property
     def length(self):
-        '''https://stackoverflow.com/a/27571588'''
+        """https://stackoverflow.com/a/27571588"""
         return(len(self.value))
 
 
     @property
     def type(self):
-        ''''''
+        """"""
         return(type(self.value))
 
 
     @property
     def sign(self):
-        ''''''
+        """"""
         return 1 if self.value[:1] == 0 else -1
 
 
@@ -59,55 +59,57 @@ class word:
 
 
     def encode(self):
-        ''''''
-        return(self.value.encode())
+        """"""
+        return self.value.encode()
 
+    def flip(self):
+        """"""
+        return self.split()[1] + self.split()[0]
 
     def to_bin(self):
-        ''''''
-        return(bin(self.to_int()))
+        """"""
+        return bin(self.to_int())
 
 
     def to_date(self):
-        '''Transform to a date in format D#YYYY-MM-DD'''
+        """Transform to a date in format D#YYYY-MM-DD"""
         dt = REF_DATE + timedelta(seconds=self.to_int())
         val = "D#{:04}-{:02}-{:02}".format(dt.year,dt.month,dt.day)
-        return(val)
+        return val
 
 
     def to_date_and_time(self):
-        '''Transform to a date and time in format DT#YYYY-MM-DD-HH-MM-SS'''
+        """Transform to a date and time in format DT#YYYY-MM-DD-HH-MM-SS"""
         dt = REF_DATE + timedelta(seconds=self.to_int())
         val = "DT#{:04}-{:02}-{:02}-{:02}:{:02}:{:02}".format(dt.year,dt.month,dt.day,dt.hour,dt.minute,dt.second)
-        return(val)
+        return val
 
     def to_int(self):
-        ''''''
-        return(int(self.value,2))
+        """"""
+        return int(self.value,2)
 
 
     def to_string(self):
-        ''''''
-        return(chr(self.to_int))
+        """"""
+        return chr(self.to_int)
 
 
     def to_time(self):
-        '''Transform to a time in the format T#MmSs'''
+        """Transform to a time in the format T#MmSs"""
         # Get the total number of seconds
-        val = self.to_int() / 100
+        v = self.to_int() / 100
         # Minutes
-        m = int(val // 60)
+        m = int(v // 60)
         # Remaining seconds
-        s = int(val % 60)
-        val = "T#{}m{}s".format(m,s)
-        return(val)
+        s = int(v % 60)
+        return "T#{}m{}s".format(m,s)
 
 
     def to_time_of_day(self):
-        '''
+        """
         Return the time of day from the total number of seconds
         https://stackoverflow.com/a/539360/
-        '''
+        """
         s = self.to_int()
         # hours
         h = s // 3600
@@ -118,12 +120,11 @@ class word:
         # remaining seconds
         s -= (m * 60)
         # total time
-        val = 'TOD#{:02}:{:02}:{:02}'.format(int(h), int(m), int(s))
-        return(val)
+        return 'TOD#{:02}:{:02}:{:02}'.format(int(h), int(m), int(s))
 
 
     def resize(self):
-        '''Resize the word to the next word size (4bit -> 16bit, 23bit -> 32bit)'''
+        """Resize the word to the next word size (4bit -> 16bit, 23bit -> 32bit)"""
         # Quotient
         q = self.length // WORD_SIZE
         # Remainder
@@ -136,17 +137,17 @@ class word:
 
 
     def shift_left(self, n):
-        ''''''
-        return(bin(self.to_int() << n)[2:].zfill(self.length))
+        """"""
+        return bin(self.to_int() << n)[2:].zfill(self.length)
 
 
     def shift_right(self, n):
-        ''''''
-        return(bin(self.to_int() >> n)[2:].zfill(self.length))
+        """"""
+        return bin(self.to_int() >> n)[2:].zfill(self.length)
 
 
     def split(self):
-        '''Split the value in half. Add a leading 0 if the length is negative'''
+        """Split the value in half. Add a leading 0 if the length is negative"""
         n = int(self.length)
         # Add 1 to n if the length is negative
         n = n + 1 if n % 2 != 0 else n
@@ -156,22 +157,20 @@ class word:
         return([v[:n],v[n:]])
 
 
-    '''
-       sign    1 bit  31
-       exp     8 bits 30-23     bias 127
-       frac   23 bits 22-0
-
-    +[3]----+[2]----+[1]----+[0]----+
-    S|  exp  |   fraction           |
-    +-------+-------+-------+-------+
-    1|<--8-->|<---23 bits---------->|
-    <-----------32 bits------------->
-    '''
     def to_float32(self):
-        '''
+        """
         Convert to a 32bit float
         https://math.stackexchange.com/a/1506364
-        '''
+        sign    1 bit  31
+        exp     8 bits 30-23     bias 127
+        frac   23 bits 22-0
+
+        +[3]----+[2]----+[1]----+[0]----+
+        S|  exp  |   fraction           |
+        +-------+-------+-------+-------+
+        1|<--8-->|<---23 bits---------->|
+        <-----------32 bits------------->
+        """
         if self.length != 32: raise ValueError(self.length)
 
         n = self.to_int()
@@ -196,7 +195,7 @@ class word:
                 return -0.0 if sign else 0.0
             else:
                 return (-1)**sign * fraction / 2**23 * 2**(-126)  # subnormal
-        elif exp == 0b1111111:
+        elif exp == 0b111111:
             if fraction == 0:
                 return float('-inf') if sign else float('inf')
             else:
@@ -205,5 +204,5 @@ class word:
 
 
     def zfill(self, n):
-        ''''''
+        """"""
         return self.value.zfill(n)
